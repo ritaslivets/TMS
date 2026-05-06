f35ae84e5294e330ef3ac8014e6ebc062ab44a6b
ritaslivets <rita4kasliva2007@gmail.com>
2026-05-06 23:59:44 +0300

    h7.done-2

--- homework 7.playground/Contents.swift
+++ homework 7.playground/Contents.swift
@@ -1,4 +1,4 @@
-//MARK: 1 classes
+//MARK: - 1 classes
 class Shape{
     let color: String
     let name: String
@@ -49,4 +49,24 @@ class Triangle: Shape {
 let newArray: [Shape] = [ Circle(radius: 4, color: "red"), Rectangle(height: 3, width: 4.5, color: "green"), Triangle(height: 3.3, side: 5, color: "blue")]
 for shape in newArray {
     print("\(shape.name) площадь: \(shape.calculateArea())")
+}
+
+
+//MARK: - 2 struct
+struct Contact {
+    let firstName: String
+    let lastName: String
+    let phoneNumber: String
+    let email: String?
+}
+func searchContacts(contacts: [Contact], searchString: String) -> [Contact] {
+    let search = searchString.lowercased()
+    return contacts.filter{ $0.firstName.lowercased().contains(search) || $0.lastName.lowercased().contains(search)}
+}
+
+let contacts = [Contact(firstName: "Misha", lastName: "Smirnov", phoneNumber: "+375291132412", email: nil ), Contact(firstName: "Oleg", lastName: "Volkolovsky", phoneNumber: "+375291121212", email: "@volk.gmail"), Contact( firstName: "Mariam", lastName: "Nojh", phoneNumber: "+6357253735", email: nil)]
+
+let result1 = searchContacts(contacts: contacts, searchString: "misha")
+for contact in result1 {
+    print("\(contact.firstName) \(contact.lastName) - \(contact.phoneNumber)")
 }
