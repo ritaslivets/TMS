bee49bc9cbdb7e881a9094f03ab7ceb0693b4761
ritaslivets <rita4kasliva2007@gmail.com>
2026-05-11 13:39:26 +0300

    //

---
+++ homework8-2экран/homework8-2экран/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework8-2экран
+//
+//  Created by Slivets Margarita on 11.05.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
